f05c8d20f0367e88a3c59faf51c10c00e3e922ac
Gennade Jean-Vincent <gennade.jv@gmail.com>
2024-11-13 21:26:38 +0700

    Solution for 1.3 Running your first JUnit test

--- src/test/java/com/spring/ApplicationTests.java
+++ src/test/java/com/spring/ApplicationTests.java
@@ -1,14 +1,19 @@
 package com.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Value("${spring.ai.openai.api-key}")
+    private String apiKey;
 
+    @Test
+    void contextLoads() {
+        assertThat(apiKey).isNotEmpty();
+    }
 }
